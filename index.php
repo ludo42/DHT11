a436a17ace8d487955afa0c80c8ad0534924d304
Ludo <ludo_42800@hotmail.fr>
2018-11-30 14:50:26 +0100

    envoi fichier

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
-<?php
-echo 'Hello World !';
+<!DOCTYPE html>
+
+
+
+<head>
+    <meta charset="utf-8">
+
+    <link href="main.css" type="text/css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <h1>Température</h1>
+
+    <?php
+    echo 'Hello World !';
+    ?>
+
+</body>
