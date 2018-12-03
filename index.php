b98e6eb8bb3e6f6983215e87e0b4b358d46c5a03
Ludo <ludo_42800@hotmail.fr>
2018-12-03 11:18:58 +0100

    modif

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-
+<html>
 
 
 <head>
     <meta charset="utf-8">
 
-    <link href="main.css" type="text/css" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="main.css">
 
 </head>
 
@@ -13,8 +13,32 @@
 
     <h1>Température</h1>
 
+
+
     <?php
-    echo 'Hello World !';
+  
+    $file='data.txt'; 
+    $contenu=file_get_contents($file); 
+    echo $contenu ;
+    $bargraph_height = 161 + $data->temperature * 4;
+    $bargraph_top = 315 + $data->temperature * 4;
     ?>
 
+    <?php
+    $data = json_decode($contenu);
+    
+    
+    ?>
+    <p>il fait
+        <?php echo $data->temperature ?>° avec
+        <?php echo $data->humidite ?>% d'humidité. </p>
+
+    <br>
+
+    <div id="thermometer">
+        <div id="bargraph" style="height: <?php echo $bargraph_height = 161 ; ?> " "top: <?php echo $bargraph_top = 315; ?>"></div>
+    </div>
+
 </body>
+
+</html>
